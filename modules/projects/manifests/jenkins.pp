e0f496aa92a60a5f62824dd61e60ca430d7c13ee
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-09-28 14:14:11 +0100

    Adding jenkins job builder project

---
+++ modules/projects/manifests/jenkins.pp
@@ -0,0 +1,36 @@
+# Jenkins jobs at ucl
+class projects::jenkins {
+  require lmod
+  $project = "jenkins"
+  $workspace = "${lmod::config::workspaces}/${project}"
+  $repodir = "${workspace}/src/${project}"
+
+  lmod::project {$project:
+    repository => 'UCL-RITS/jenkins-job-builder-files',
+    python     => 3,
+    modlines   => [
+      "set_alias(\"production\", \"jenkins-jobs --conf ${workspace}/.production.ini\")",
+      "set_alias(\"staging\", \"jenkins-jobs --conf ${workspace}/.staging.ini\")",
+    ]
+  }
+  misc::pip {
+    "${project}-jenkins-job-builder":
+      prefix  => $workspace,
+      package => 'jenkins-job-builder',
+      require => Lmod::Project[$project];
+    "${project}-jenkjobs":
+      prefix  => $workspace,
+      package => 'git+https://github.com/UCL/jenkjobs',
+      require => Lmod::Project[$project];
+  }
+
+  file {
+    "${workspace}/.staging.ini":
+      content => template("projects/jenkins/staging.ini.erb"),
+      require => Lmod::Project[$project];
+    "${workspace}/.production.ini":
+      content => template("projects/jenkins/production.ini.erb"),
+      require => Lmod::Project[$project]
+  }
+
+}
