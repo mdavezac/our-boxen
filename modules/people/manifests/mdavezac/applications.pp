8104463ab1c6fd85ca8a3a0137fb6127f3f6d350
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-09-29 15:08:21 +0100

    Adding pandoc and tex

--- modules/people/manifests/mdavezac/applications.pp
+++ modules/people/manifests/mdavezac/applications.pp
@@ -6,14 +6,18 @@ class people::mdavezac::applications {
   include brewcask
   include homebrew
 
-  package { 'skype': provider           => 'brewcask' }
-  package { 'slack': provider           => 'brewcask' }
-  package { 'bettertouchtool': provider => 'brewcask' }
-  package { 'the_silver_searcher': }
-  package { 'lua': }
-  package { 'luajit': }
-  package { 'cmake': }
-  package { 'dropbox': provider    => 'brewcask' }
-  package { 'firefox': provider    => 'brewcask' }
-  package { 'zotero': provider    => 'brewcask' }
+  package {
+    'skype': provider => brewcask;
+    'slack': provider => brewcask;
+
+    'bettertouchtool': provider => brewcask;
+
+    'cmake':;
+    'the_silver_searcher':;
+    'lua':;
+    'luajit':;
+
+    'mactex': provider =>  brewcask;
+    'pandoc':;
+  }
 }
