16d16cf63abf1068b141c54b6bd1a2b4a0622874
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-09-29 18:10:12 +0100

    Adding dotfiles as an lmod project

--- modules/people/manifests/mdavezac.pp
+++ modules/people/manifests/mdavezac.pp
@@ -3,25 +3,29 @@ class people::mdavezac {
   git::config::global { 'user.email': value  => 'm.davezac@ucl.ac.uk' }
   git::config::global { 'user.name': value  => 'Mayeul d\'Avezac' }
 
-  repository { "/Users/${::boxen_user}/.dotfiles":
-    source => "${::github_login}/dotfiles",
-  } -> file { "/Users/${::boxen_user}/.hgrc":
-    ensure => 'link',
-    target => "/Users/${::boxen_user}/.dotfiles/hgrc"
-  } -> file { "/Users/${::boxen_user}/.ctags":
-    ensure => 'link',
-    target => "/Users/${::boxen_user}/.dotfiles/ctags"
+  require lmod
+  lmod::project { 'dotfiles':
+    repository => "mdavezac/SIUnits.jl",
+    source_dir => "/Users/${::boxen_user}/.dotfiles",
+  } -> file {
+    "/Users/${::boxen_user}/.hgrc":
+      ensure  => 'link',
+      target  => "/Users/${::boxen_user}/.dotfiles/hgrc";
+    "/Users/${::boxen_user}/.ctags":
+      ensure => 'link',
+      target => "/Users/${::boxen_user}/.dotfiles/ctags",
   }
+
   include osx::dock::autohide
   include osx::global::natural_mouse_scrolling
   include osx::global::enable_standard_function_keys
   include osx::global::tap_to_click
   include osx::software_update
-  class { 'osx::mouse::swipe_between_pages':
-    enabled => true
-  }
-  class { 'osx::mouse::button_mode':
-    mode => 2
+  class {
+    'osx::mouse::swipe_between_pages':
+      enabled => true;
+    'osx::mouse::button_mode':
+      mode => 2
   }
 
   include people::mdavezac::keyboard
