920f6cab2219887542c94eaa746a6bc596889771
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-10-06 13:26:44 +0100

    Adding slack to jenkins

--- modules/projects/manifests/jenkins.pp
+++ modules/projects/manifests/jenkins.pp
@@ -7,11 +7,14 @@ class projects::jenkins {
 
   lmod::project {$project:
     repository => 'UCL-RITS/jenkins-job-builder-files',
-    python     => 3,
+    python     => 2,
     modlines   => [
       "set_alias(\"production\", \"jenkins-jobs --conf ${workspace}/.production.ini\")",
       "set_alias(\"staging\", \"jenkins-jobs --conf ${workspace}/.staging.ini\")",
     ]
+  } -> repository { "${project}-rc-puppet":
+    path   => "${workspace}/src/rc-puppet",
+    source => 'UCL-RITS/rc_puppet.git'
   }
   misc::pip {
     "${project}-jenkins-job-builder":
@@ -20,7 +23,11 @@ class projects::jenkins {
       require => Lmod::Project[$project];
     "${project}-jenkjobs":
       prefix  => $workspace,
-      package => 'git+https://github.com/UCL/jenkjobs',
+      package => 'git+https://github.com/UCL/jenkjobs==1.2.0',
+      require => Lmod::Project[$project];
+    "${project}-slack":
+      prefix  => $workspace,
+      package => 'git+https://github.com/asmundg/jenkins-jobs-slack.git',
       require => Lmod::Project[$project];
   }
 
@@ -34,6 +41,24 @@ class projects::jenkins {
     "${workspace}/.vimrc":
       content => template("projects/jenkins/vimrc.erb"),
       require => Lmod::Project[$project];
+    "${workspace}/src/jenkins/branch.yaml":
+      content => "production",
+      require => Lmod::Project[$project];
+    "${workspace}/src/jenkins/jenkinsdescription.yaml":
+      content => "UCL RSD Jenkins",
+      require => Lmod::Project[$project];
+    "${workspace}/src/jenkins/ucl-rits-slack-token":
+      ensure  => link,
+      target  => "/Users/${::boxen_user}/.secrets/ucl-rits-slack-token",
+      require => Lmod::Project[$project];
+    "${workspace}/src/jenkins/purify-slack-token":
+      ensure  => link,
+      target  => "/Users/${::boxen_user}/.secrets/purify-slack-token",
+      require => Lmod::Project[$project];
+    "${workspace}/bin/build.zsh":
+      ensure  => file,
+      content => "ssh jenkins_legion \"bash -l\" < \$1",
+      mode    => 700;
   }
 
   repository { "${workspace}/src/build_files":
