fc511ffe38a8be2571dbc1ba4a8b00e13aabe58f
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-10-12 10:12:58 +0100

    Adding tarfile as a possible source

--- modules/misc/manifests/cmake.pp
+++ modules/misc/manifests/cmake.pp
@@ -2,18 +2,32 @@ define misc::cmake(
   $url,
   $source_dir,
   $prefix,
+  $tarfile=false,
   $options=""
 ) {
   $cmake_options = "-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=${prefix} $options"
-  repository { "${name}-repo":
-    source  => $url,
-    path    => $source_dir,
-  } -> file { "${source_dir}/build":
-    ensure => directory
-  } -> exec { "${name}-configure":
+  if( $tarfile == false ) {
+    repository { "${name}-repo":
+      source  => $url,
+      path    => $source_dir,
+    } -> file { "${source_dir}/build":
+      ensure => directory
+    }
+  } else {
+    exec { "${name}-repo":
+      command => "curl -L $url | tar xv",
+      cwd     => "${prefix}/src",
+      creates => $source_dir
+    } -> file { "${source_dir}/build":
+      ensure => directory
+    }
+  }
+
+  exec { "${name}-configure":
     command => "cmake .. -G Ninja ${cmake_options}",
     cwd     => "${source_dir}/build",
     creates => "${source_dir}/build/CMakeCache.txt",
+    require => File["${source_dir}/build"]
   } -> exec { "${name}-install":
     command => 'ninja -j4 install',
     cwd     => "${source_dir}/build",
