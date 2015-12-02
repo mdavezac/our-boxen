e036c3b193cb57e6ef7752cec758c44906f99b67
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-12-02 13:33:02 +0000

    Updating optimet.pp

--- modules/projects/manifests/optimet.pp
+++ modules/projects/manifests/optimet.pp
@@ -5,12 +5,10 @@ class projects::optimet($project='optimet') {
   $workspace = "${lmod::config::workspaces}/${project}"
 
   lmod::project {$project:
+    repository => "OPTIMET/OPTIMET"
   }
 
   file {
-    "${workspace}/src":
-      ensure => directory,
-      require => Lmod::Project[$project];
     "${workspace}/.vimrc":
       ensure  => file,
       content => template("projects/${project}/vimrc.erb"),
@@ -23,22 +21,61 @@ class projects::optimet($project='optimet') {
 
   exec { "${project} - Download gsl":
     command => 'curl -L http://ftpmirror.gnu.org/gsl/gsl-1.16.tar.gz | tar xv',
- creates => "${workspace}/src/gsl-1.16",
+    creates => "${workspace}/src/gsl-1.16",
     cwd     => "${workspace}/src",
-    require => File["${workspace}/src"]
-  } -> exec { "${project} - install":
+    require => Lmod::Project[$project];
+  } -> exec { "${project} - gsl install":
     command => "zsh -lc \"./configure --prefix=${workspace} && make && make install -j8\"",
     cwd     => "${workspace}/src/gsl-1.16",
     path    => '/opt/boxen/homebrew/bin',
     creates => "${workspace}/lib/libgsl.dylib"
   }
 
+  exec { "${project} - Download hdf5":
+    command => 'curl -L http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar | tar xv',
+    creates => "${workspace}/src/hdf5-1.8.16",
+    cwd     => "${workspace}/src",
+    require => Lmod::Project[$project];
+  } -> exec { "${project} - hdf5 install":
+    command => "zsh -lc \"./configure --prefix=${workspace} && make && make install -j8\"",
+    cwd     => "${workspace}/src/hdf5-1.8.16",
+    path    => '/opt/boxen/homebrew/bin',
+    creates => "${workspace}/lib/libhdf5.dylib"
+  }
+
+  exec { "${project} - Download f2c":
+    command => 'curl -L http://www.netlib.org/f2c/libf2c.zip -o libf2c.zip ',
+    creates => "${workspace}/src/libf2c.zip",
+    cwd     => "${workspace}/src",
+    require => Lmod::Project[$project];
+  } -> exec { "${project} - unzip f2c":
+    command => 'unzip libf2c.zip -d libf2c',
+    creates => "${workspace}/src/libf2c",
+    cwd     => "${workspace}/src"
+  } -> exec { "${project} make f2c.h":
+    command => 'make -f makefile.u hadd && make -f makefile.u f2c.h',
+    creates => "${workspace}/src/libf2c/f2c.h",
+    cwd     => "${workspace}/src/libf2c"
+  } -> exec { "${project} make libf2c":
+    command => 'make -f makefile.u all',
+    creates => "${workspace}/src/libf2c/libf2c.a",
+    cwd     => "${workspace}/src/libf2c"
+  } -> file {
+    "${workspace}/include/f2c.h":
+      ensure => file,
+      source => "${workspace}/src/libf2c/f2c.h";
+    "${workspace}/lib/libf2c.a":
+      ensure => file,
+      source => "${workspace}/src/libf2c/libf2c.a";
+  }
+
+
   misc::cmake { "${project}-armadillo":
     url        => "https://downloads.sourceforge.net/project/arma/armadillo-6.100.0.tar.gz",
     source_dir => "${workspace}/src/armadillo-6.100.0",
     prefix     => $workspace,
     tarfile    => true,
-    require => File["${workspace}/src"]
+    require =>  Lmod::Project[$project]
   }
 
 }
